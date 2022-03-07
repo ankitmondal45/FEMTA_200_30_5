e0870d61e06e1bbc0966a7a22d158e6104decbe7
ankitmondal45 <82613905+ankitmondal45@users.noreply.github.com>
2022-03-07 14:38:52 -0500

    Add files via upload

--- FEMTA_200_30_5_V2.m
+++ FEMTA_200_30_5_V2.m
@@ -10,6 +10,8 @@ XP = 40;
 YP = 35;
 ZP = 6;
 
+% since facing was 0.25 need depth of 6.25 when part is cut
+
 % Aluminum stock
 XS = 100.75;
 YS = 63.5;
@@ -52,7 +54,7 @@ fclose(file);
 N = 1;
 b = 8;
 
-Ps = [10, 38; 10,-3]; % need to double check
+Ps = [21.525/2 + 4, 38/2; 21.525/2 + 4,-38/2]; % need to double check
 Rs = [0];
 Ds = [0];
 Fls = [250];
@@ -61,24 +63,33 @@ Fls = [250];
 
 %quarter round functions 
 
-file = fopen('./gcode/FEMTA_200_30_5_OP4_3mmQM_SMAX.tap', 'w')
+file = fopen('./gcode/FEMTA_200_30_5_OP4_3mmQM_SMAX.tap', 'w');
 % RHS Polygroove (Quarter Round)
-N = poly_groove(file, N, b, 150, Fls, [3.525, -17.5, 0], Ps, Rs, Ds, -3, 0, 0.05, 3, 3, true, true, false);
-Ps = [32,38; 32,-3];
-N = poly_groove(file, N, b, 150, Fls, [3.525, -17.5, 0], Ps, Rs, Ds, 3, 0, 0.05, 3, 3, false, true, false);
+N = poly_groove(file, N, b, 25, Fls, [0, 0, 0], Ps, Rs, Ds, -3, 0, 0.05, 3, 3, true, true, false);
+Ps = [-21.525/2 - 4, 38/2; -21.525/2 - 4,-38/2];
+N = poly_groove(file, N, b, 25, Fls, [0, 0, 0], Ps, Rs, Ds, 3, 0, 0.05, 3, 3, false, true, false);
 
 % LHS Polygroove (Quarter Round)
-Ps = [-32,38; -32,-3];
-N = poly_groove(file, N, b, 150, Fls, [-3.525, -17.5, 0], Ps, Rs, Ds, -3, 0, 0.05, 3, 3, false, true, false);
-Ps = [-10, 38; -10,-3];
-N = poly_groove(file, N, b, 150, Fls, [-3.525, -17.5, 0], Ps, Rs, Ds, 3, 0, 0.05, 3, 3, false, true, true);
+Ps = [21.525/2 + 24, 38/2; 21.525/2 + 24,-38/2];
+N = poly_groove(file, N, b, 25, Fls, [0, 0, 0], Ps, Rs, Ds, 3, 0, 0.05, 3, 3, false, true, false);
+Ps = [-21.525/2 - 24, 38/2; -21.525/2 - 24,-38/2];
+N = poly_groove(file, N, b, 25, Fls, [0, 0, 0], Ps, Rs, Ds, -3, 0, 0.05, 3, 3, false, true, true);
 fclose(file);
 
-%% Make cuts between parts
-N = 1;
-b = 3/8*25.4;
-
-file = fopen('./gcode/FEMTA_200_30_5_OP5_ZP_38inEM_SMAX.tap','w');
-N = surface_face(file, N, b, Fd, Flmax, [0, 0, 0], 0.7, 0.001, YS, 1, 10, dz, true, true, true);
+%% Face off remaining stock from parts
+file = fopen('./gcode/FEMTA_200_30_5_OP7_ZP_38inEM_SMAX.tap','w');
+N = surface_face(file, N, b, Fd, Flmax, [0, 0, 0], 0.7, XP, YP, 1, ZS-ZP-0.25, dz, true, true, true);
 fclose(file);
 
+%% Cut out parts
+b = 6;
+b1 = 3/8*25.4;
+Ps = [-40 - b/2, 35/2 + b/2; b - b1, 35/2 + b/2 ; b - b1, -35/2 - b/2; -40 - b/2, -35/2 - b/2; -40 - b/2, 35/2 + b/2];
+Rs = [0 0 0 0];
+Ds = [0 0 0 0];
+Fls = [250 250 250 250];
+file = fopen('./gcode/FEMTA_200_30_5_OP8_ZP_38inEM_SMAX.tap','w');
+N = poly_groove(file, N, b, 25, Fls, [0, 0, 0], Ps, Rs, Ds, 0, 0, 1, 9.8, 0.22, true, true, false);
+Ps = [b1 - b, 35/2 + b/2; 40 + b/2, 35/2 + b/2 ; 40 + b/2, -35/2 - b/2; b1 - b, -35/2 - b/2; b1 - b, 35/2 + b/2];
+N = poly_groove(file, N, b, 25, Fls, [0, 0, 0], Ps, Rs, Ds, 0, 0, 1, 9.8, 0.22, false, true, true);
+fclose(file);
